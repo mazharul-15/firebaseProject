33a0e7f842294595865bae9829a11ffc5efdc613
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-05-13 11:36:44 +0600

    adding firebase, glide & circle image dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
